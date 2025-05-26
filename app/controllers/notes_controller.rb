238e2bb1b93b945a3fc627ba3a7d55012b94c84d
valentin <val66450@gmail.com>
2025-05-26 20:45:49 +0200

    UPDATE - Rien ne va plus

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,8 @@
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  skip_forgery_protection only: [:new,:edit]
+  # protect_from_forgery except: :edit
   def index
     @notes = Note.all
   end
@@ -9,30 +11,44 @@ class NotesController < ApplicationController
   end
 
   def new
-    @note = current_user.notes.build
+    @note = Note.new
+    respond_to do |format|
+      format.js { render :form }
+    end
   end
 
   def edit
+    @note = Note.find(params[:id])
+    respond_to do |format|
+      format.js { render :form }
+    end
   end
 
   def create
     @note = current_user.notes.build(note_params)
     respond_to do |format|
       if @note.save
+        format.js
         format.html { redirect_to note_url(@note), notice: 'Note was successfully created.' }
       else
+        format.js { render :form, status: :unprocessable_entity }
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
 
   def update
-    if @note.update(note_params)
-      redirect_to @note, notice: 'Note was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @note.update(note_params)
+        format.js
+        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
+      else
+        format.js { render :form, status: :unprocessable_entity }
+        format.html { render :edit }
+      end
     end
   end
+
 
   def destroy
     @note.destroy
